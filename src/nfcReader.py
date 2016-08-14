c2f317a0184e49c9961e9350c1db0d9bc8f79083
Antoine <antoine.epitech@gmail.com>
2016-08-14 16:52:21 -0700

    strucute nfcreader into class

--- src/nfcReader.py
+++ src/nfcReader.py
@@ -25,76 +25,88 @@ import string
 
 import Adafruit_PN532 as PN532
 
-printable = set(string.printable)
 
-# Setup how the PN532 is connected to the Raspbery Pi/BeagleBone Black.
-# It is recommended to use a software SPI connection with 4 digital GPIO pins.
 
-# Configuration for a Raspberry Pi:
-CS = 18
-MOSI = 23
-MISO = 24
-SCLK = 25
+class Reader():
 
-# Configuration for a BeagleBone Black:
-# CS   = 'P8_7'
-# MOSI = 'P8_8'
-# MISO = 'P8_9'
-# SCLK = 'P8_10'
+    def __init__(self):
+        printable = set(string.printable)
 
-# Create an instance of the PN532 class.
-pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
+class NFCReader(Reader):
 
-# Call begin to initialize communication with the PN532.  Must be done before
-# any other calls to the PN532!
-pn532.begin()
+    _CS = 0
+    _MOSI = 0
+    _MISO = 0
+    _SCLK = 0
+    printable = ""
 
-# Get the firmware version from the chip and print(it out.)
-ic, ver, rev, support = pn532.get_firmware_version()
-print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+    def __init__(self):
+        self.printable = set(string.printable)
 
-# Configure PN532 to communicate with MiFare cards.
-pn532.SAM_configuration()
 
-# Main loop to detect cards and read a block.
-print('Waiting for MiFare card...')
-while True:
-    # Check if a card is available to read.
-    uid = pn532.read_passive_target()
-    # Try again if no card is available.
-    if uid is None:
-        continue
-    print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-    # Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
-    # if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
-    #                                               [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
-    #    print('Failed to authenticate block 4!')
-    #    continue
-    # Read block 4 data.
-    i = 7
-    payload = ""
-    try:
-        while pn532.mifare_classic_read_block(i) is not None:
-            data = pn532.mifare_classic_read_block(i)
-            buffer = format(binascii.hexlify(data[:4]))
-            buffer = binascii.unhexlify(buffer)
-            print(buffer)
-            payload = payload + buffer
-            # print((binascii.hexlify(data[:8]).decode('hex')))
-            i = i + 1
+        # Setup how the PN532 is connected to the Raspbery Pi/BeagleBone Black.
+        # It is recommended to use a software SPI connection with 4 digital GPIO pins.
 
-        payload = filter(lambda x: x in printable, payload)
-        print (payload)
-    except:
-        print ("reading error")
-        # Note that 16 bytes are returned, so only show the first 4 bytes for the block.
-        # print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
-        # Example of writing data to block 4.  This is commented by default to
-        # prevent accidentally writing a card.
-        # Set first 4 bytes of block to 0xFEEDBEEF.
-        # data[0:4] = [0xFE, 0xED, 0xBE, 0xEF]
-        # # Write entire 16 byte block.
-        # pn532.mifare_classic_write_block(4, data)
-        # print('Wrote to block 4, exiting program!')
-        # # Exit the program to prevent continually writing to card.
-        # sys.exit(0)
+        # Configuration for a Raspberry Pi:
+        self._CS = 18
+        self._MOSI = 23
+        self._MISO = 24
+        self._SCLK = 25
+
+    def listen(self):
+        # Create an instance of the PN532 class.
+        pn532 = PN532.PN532(cs=self._CS, sclk=self._SCLK, mosi=self._MOSI, miso=self._MISO)
+
+        # Call begin to initialize communication with the PN532.  Must be done before
+        # any other calls to the PN532!
+        pn532.begin()
+
+        # Get the firmware version from the chip and print(it out.)
+        ic, ver, rev, support = pn532.get_firmware_version()
+        print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+
+        # Configure PN532 to communicate with MiFare cards.
+        pn532.SAM_configuration()
+
+        # Main loop to detect cards and read a block.
+        print('Waiting for MiFare card...')
+        while True:
+            # Check if a card is available to read.
+            uid = pn532.read_passive_target()
+            # Try again if no card is available.
+            if uid is None:
+                continue
+            print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
+            # Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
+            # if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
+            #                                               [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
+            #    print('Failed to authenticate block 4!')
+            #    continue
+            # Read block 4 data.
+            i = 7
+            payload = ""
+            try:
+                while pn532.mifare_classic_read_block(i) is not None:
+                    data = pn532.mifare_classic_read_block(i)
+                    buffer = format(binascii.hexlify(data[:4]))
+                    buffer = binascii.unhexlify(buffer)
+                    print(buffer)
+                    payload = payload + buffer
+                    # print((binascii.hexlify(data[:8]).decode('hex')))
+                    i = i + 1
+
+                payload = filter(lambda x: x in self.printable, payload)
+                print (payload)
+            except:
+                print ("reading error")
+                # Note that 16 bytes are returned, so only show the first 4 bytes for the block.
+                # print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+                # Example of writing data to block 4.  This is commented by default to
+                # prevent accidentally writing a card.
+                # Set first 4 bytes of block to 0xFEEDBEEF.
+                # data[0:4] = [0xFE, 0xED, 0xBE, 0xEF]
+                # # Write entire 16 byte block.
+                # pn532.mifare_classic_write_block(4, data)
+                # print('Wrote to block 4, exiting program!')
+                # # Exit the program to prevent continually writing to card.
+                # sys.exit(0)
