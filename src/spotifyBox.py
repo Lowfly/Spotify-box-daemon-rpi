d4022840d6cfbd5d8f4b3f2e1f4d3b5196843fb0
Antoine <antoine.epitech@gmail.com>
2016-08-14 17:15:45 -0700

    does it work

--- src/spotifyBox.py
+++ src/spotifyBox.py
@@ -13,20 +13,74 @@ commands.
 
 from __future__ import unicode_literals
 
-import cmd
 import logging
-import threading
 
 import spotifySDK
-from subprocess import call
+import spotify
 
+import binascii
+import sys
+import string
 
 
 class SpotifyBox():
 
-    _sdk = spotifySDK();
+    _sdk = ""
+    _nfcReader = ""
+    _config = ""
 
     def __init__(self):
+        print("Log | _init_SpotifyBox_ | Start")
+        self._config = spotify.Config()
+        self._config.load_application_key_file('spotify_appkey.key')
+        print("Log | _init_SpotifyBox_ | End Success")
 
-    def listen(self):
-        print("toto");
+
+
+    def run(self):
+        logging.basicConfig(level=logging.INFO)
+        _sdk = spotifySDK.SpotifySDK()
+        _sdk.do_play_uri('spotify:track:4n4Z8qVlDkEnfX68PYJfJu')
+        _sdk.listen()
+        #_sdk.cmdloop()
+        # Main loop to detect cards and read a block.
+        print('Waiting for MiFare card...')
+        while True:
+                # Check if a card is available to read.
+            uid = self.reader.read_passive_target()
+                # Try again if no card is available.
+            if uid is None:
+                continue
+            print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
+                # Authenticate block 4 for reading with default key (0xFFFFFFFFFFFF).
+                # if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
+                #                                               [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
+                #    print('Failed to authenticate block 4!')
+                #    continue
+                # Read block 4 data.
+            i = 7
+            payload = ""
+            try:
+                while self.reader.mifare_classic_read_block(i) is not None:
+                    data = self.reader.mifare_classic_read_block(i)
+                    buffer = format(binascii.hexlify(data[:4]))
+                    buffer = binascii.unhexlify(buffer)
+                    print(buffer)
+                    payload = payload + buffer
+                    # print((binascii.hexlify(data[:8]).decode('hex')))
+                    i = i + 1
+                payload = filter(lambda x: x in self.printable, payload)
+                print (payload)
+            except :
+                print ("reading error")
+                    # Note that 16 bytes are returned, so only show the first 4 bytes for the block.
+                    # print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+                    # Example of writing data to block 4.  This is commented by default to
+                    # prevent accidentally writing a card.
+                    # Set first 4 bytes of block to 0xFEEDBEEF.
+                    # data[0:4] = [0xFE, 0xED, 0xBE, 0xEF]
+                    # # Write entire 16 byte b   lock.
+                    # pn532.mifare_classic_write_block(4, data)
+                    # print('Wrote to block 4, exiting program!')
+                    # # Exit the program to prevent continually writing to card.
+                    # sys.exit(0)
