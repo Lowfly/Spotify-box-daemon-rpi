67b08170c9c8dd2468d8652aac78332af0e40a8b
Antoine <antoine.epitech@gmail.com>
2016-08-16 13:41:21 -0700

    api

---
+++ src/spotifyAPI.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+This is an example of a simple command line client for Spotify using pyspotify.
+
+You can run this file directly::
+
+    python shell.py
+
+Then run the ``help`` command on the ``spotify>`` prompt to view all available
+commands.
+"""
+
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+
+
+class SpotifyAPI():
+
+    base_url = "https://api.spotify.com/v1"
+    tracklist = []
+
+    def __init__(self):
+        pass
+
+    def getAlbum(self, spotify_uri):
+        url = self.base_url + "/albums/" + spotify_uri
+        response = requests.get(url, verify=True)
+        print (response)
+
+        if (response.ok):
+
+            jData = json.loads(response.content)
+            for track in jData["tracks"]["items"]:
+                self.tracklist.append(track["uri"])
+            return self.tracklist
+
+        else:
+            response.raise_for_status()
+
+    #def getPlaylist(self):
+
+    #def getDetails(self):
+
